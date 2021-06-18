ff62f68d91d374c853711066a453f8ff737a5165
payel199 <payelpaul616@gmail.com>
2021-06-18 15:55:17 +0530

    Dark_Mode_Enable

---
+++ app/src/main/java/com/examplepyl/dark/MainActivity.java
@@ -0,0 +1,40 @@
+package com.examplepyl.dark;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+    Switch toggleButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.Theme_Dark);
+        }else {
+            setTheme(R.style.Theme_Light);
+        }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toggleButton=findViewById(R.id.toggleButton);
+        toggleButton.setTextOn("On");
+        toggleButton.setTextOff("Off");
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+                }
+            }
+        });
+    }
+}
